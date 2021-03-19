8a235acb5b3685e37235a01672b961030439baa6
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-03-19 15:16:20 +0530

    Create Program_One.kt

---
+++ MADA_Program/app/src/main/java/com/pratik/mada_program/Program_One.kt
@@ -0,0 +1,11 @@
+package com.pratik.mada_program
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Program_One : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_program__one)
+    }
+}
