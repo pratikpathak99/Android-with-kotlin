57ae1120e3c43a142c1a0e6e33bd843e8a5180a4
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-05-29 12:32:52 +0530

    Create Program_nine.kt

---
+++ MADA_Program/app/src/main/java/com/pratik/mada_program/Program_nine.kt
@@ -0,0 +1,13 @@
+package com.pratik.mada_program
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Program_nine : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_program_nine)
+
+        supportActionBar?.hide()
+    }
+}
