9c26657786f8e953a6802053fdbc36863e3e91c7
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-05-30 20:04:18 +0530

    Create afterNotification.kt

---
+++ MADA_Program/app/src/main/java/com/pratik/mada_program/afterNotification.kt
@@ -0,0 +1,11 @@
+package com.pratik.mada_program
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class afterNotification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_notification)
+    }
+}
