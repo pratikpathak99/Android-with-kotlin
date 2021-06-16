418dd2b3709df8477f8ed2471f1938cd721941d0
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-06-16 18:17:13 +0530

    Create program_two_part_three.kt

---
+++ MADA_Program/app/src/main/java/com/pratik/mada_program/program_two_part_three.kt
@@ -0,0 +1,19 @@
+package com.pratik.mada_program
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class program_two_part_three : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_program_two_part_three)
+        supportActionBar?.hide()
+    }
+
+    fun shared_preference(view: View) {
+        intent = Intent(applicationContext,shared_preference::class.java)
+        startActivity(intent);
+    }
+}
