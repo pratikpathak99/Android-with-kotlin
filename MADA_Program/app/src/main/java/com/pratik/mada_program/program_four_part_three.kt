435e34015aae6e07e4b909adc6cf4041c0c29511
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-06-17 12:12:10 +0530

    Create program_four_part_three.kt

---
+++ MADA_Program/app/src/main/java/com/pratik/mada_program/program_four_part_three.kt
@@ -0,0 +1,13 @@
+package com.pratik.mada_program
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class program_four_part_three : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_program_four_part_three)
+
+        supportActionBar?.hide()
+    }
+}
