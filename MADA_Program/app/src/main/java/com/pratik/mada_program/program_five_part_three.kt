ce6c287734511efb38bff74b3235dcdf30d0e579
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-07-08 19:07:15 +0530

    Create program_five_part_three.kt

---
+++ MADA_Program/app/src/main/java/com/pratik/mada_program/program_five_part_three.kt
@@ -0,0 +1,11 @@
+package com.pratik.mada_program
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class program_five_part_three : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_program_five_part_three)
+    }
+}
